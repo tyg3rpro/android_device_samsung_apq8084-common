a302ab992ce346d225f5a8a663480251d3cb45e6
Say <sktjdgns1189@naver.com>
2016-01-27 02:17:00 +0900

    Use opensource librmnetctl

--- apq8084.mk
+++ apq8084.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsecnativefeature
 
+# RMNET
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 # Thermal engine
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
